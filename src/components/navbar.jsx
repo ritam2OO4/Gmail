5e91aa1d4566079c74e9eeb7d9954498b0fb7dd7
Ritam_devs <ritamdevs2004@gmail.com>
2024-09-06 20:35:05 +0530

    completion_gmail

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,53 +1,90 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { RxHamburgerMenu } from "react-icons/rx";
-import { FaRegQuestionCircle } from "react-icons/fa";
+import React, { useEffect, useState } from 'react'
 import { IoSettingsOutline } from "react-icons/io5";
-import { TbGridDots } from "react-icons/tb";
-import { faUserCircle, faSearch } from '@fortawesome/free-solid-svg-icons';
-import GoogleLogo from './googleLogo';
+import { FaRegQuestionCircle } from "react-icons/fa";
+import { PiDotsNineBold } from "react-icons/pi";
+import { IoIosSearch } from "react-icons/io";
+import Avatar from 'react-avatar';
+import { RxHamburgerMenu } from "react-icons/rx";
+import { useDispatch, useSelector } from 'react-redux';
+import { setAuthUser, setSearchText } from '../redux/appSlice';
+import { auth } from '../firebase';
+import { signOut } from 'firebase/auth';
+import { motion, AnimatePresence } from 'framer-motion';
 
-function Navbar() {
-    return (
-        <nav className="bg-[#F6F8FC] shadow-md px-4 py-2 flex items-center justify-between">
-            <div className="flex items-center justify-center mr-0 pr-0">
-                <RxHamburgerMenu size={'2.8rem'} className="hover:bg-[#E8EAED] rounded-full p-2 cursor-pointer" />
-                {/* Google Logo */}
-                <div className="p-2 w-[100px]">
-                    <GoogleLogo />
-                </div>
 
-                {/* Navigation Links */}
-                <div className="hidden md:flex space-x-4">
-                    <a href="#" className="text-gray-700 hover:text-blue-500">Inbox</a>
-                    <a href="#" className="text-gray-700 hover:text-blue-500">Starred</a>
-                    <a href="#" className="text-gray-700 hover:text-blue-500">Sent</a>
-                    <a href="#" className="text-gray-700 hover:text-blue-500">Drafts</a>
-                </div>
-            </div>
+const Navbar = () => {
+  const [search, setSearch] = useState("");
+  const [toggle, setToggle] = useState(false);
+  const dispatch = useDispatch();
+  const { authUser } = useSelector(store => store.app);
 
-            {/* Search Bar */}
-            <div className="flex-1 mx-4 max-w-2xl">
-                <div className="relative">
-                    <FontAwesomeIcon  icon={faSearch} className=" hover:bg-[#E8EAED] rounded-full p-2 cursor-pointer absolute top-4 left-2 text-gray-600 px-1" />
-                    <input
-                        type="text"
-                        placeholder="Search mail"
-                        className="w-full px-8 py-2 pt-4 border rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500 bg-[#EAF1FB]"
-                    />
-                </div>
-            </div>
+  const signOutHandler = () => {
+    signOut(auth).then(() => {
+      dispatch(setAuthUser(null));
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+  useEffect(() => {
+    dispatch(setSearchText(search));
+  }, [search]);
 
-            {/* Icons */}
-            <div className="flex items-center space-x-4">
-                <FaRegQuestionCircle className="text-gray-700 hover:bg-[#E8EAED] rounded-full p-2 cursor-pointer hover:text-blue-500" size={'2.5rem'} />
-                <IoSettingsOutline className="text-gray-700 hover:bg-[#E8EAED] rounded-full p-2 cursor-pointer hover:text-blue-500" size={'2.5rem'} />
-                <TbGridDots className="text-gray-700 hover:bg-[#E8EAED] rounded-full p-2 cursor-pointer hover:text-blue-500" size={'2.5rem'} />
-                 <FontAwesomeIcon icon={faUserCircle} className="text-gray-700 hover:bg-[#E8EAED] rounded-full p-2 cursor-pointer hover:text-blue-500 text-2xl" size={'2.8rem'} /> 
-                  {/* // we can use npm packadge for adding avatar as react-avatar which take src as avatar or we can use img tag too  */}
-            </div>
-        </nav>
-    );
+
+  return (
+    <div className='flex items-center justify-between mx-3 h-16'>
+      <div className='flex items-center gap-10'>
+        <div className='flex items-center gap-2'>
+          <div className='p-3 rounded-full hover:bg-gray-100 cursor-pointer'>
+            <RxHamburgerMenu size={'20px'} />
+          </div>
+          <img className='w-8' src={"https://mailmeteor.com/logos/assets/PNG/Gmail_Logo_512px.png"} alt="" />
+          <h1 className='text-2xl text-gray-500 font-medium'>Gmail</h1>
+        </div>
+      </div>
+      <div className='md:block hidden w-[50%] mr-60'>
+        <div className='flex items-center bg-[#EAF1FB] px-2 py-3  rounded-full'>
+          <IoIosSearch size="24px" className='text-gray-700' />
+          <input
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder='Seach mail'
+            className='rounded-full w-full bg-transparent outline-none px-1'
+          />
+        </div>
+      </div>
+      <div className='md:block hidden'>
+        <div className='flex items-center gap-2'>
+          <div className='p-3 rounded-full hover:bg-gray-100 cursor-pointer'>
+            <FaRegQuestionCircle size={"20px"} />
+          </div>
+          <div className='p-3 rounded-full hover:bg-gray-100 cursor-pointer'>
+            <IoSettingsOutline size={"20px"} />
+          </div>
+          <div className='p-3 rounded-full hover:bg-gray-100 cursor-pointer'>
+            <PiDotsNineBold size={"20px"} />
+          </div>
+          <div className='relative cursor-pointer'>
+            <Avatar onClick={() => setToggle(!toggle)} src={authUser?.photoURL} googleId="118096717852922241760" size="40" round={true} />
+            <AnimatePresence>
+              {
+                toggle && (
+                  <motion.div
+                    initial={{ opacity: 0, scale: 0.8 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    exit={{ opacity: 0, scale: 0.8 }}
+                    transition={{ duration: 0.1 }}
+                    className='absolute right-2 z-20 shadow-lg bg-white rounded-md'>
+                    <p onClick={signOutHandler} className='p-2 underline'>LogOut</p>
+                  </motion.div>
+                )
+              }
+            </AnimatePresence>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default Navbar;
+export default Navbar
